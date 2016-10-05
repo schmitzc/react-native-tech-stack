bfb11afd6b1869cdeeb46e2e35ad6afb3b891c90
Cassie Koomjian <schmitzc@gmail.com>
2016-10-05 11:41:30 -0700

    Add LibraryReducer and LibraryList component

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import { View } from 'react-native';
 
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => (
   <Provider store={createStore(reducers)}>
     <View>
       <Header headerText='Tech Stack' />
+      <LibraryList />
     </View>
   </Provider>
 );
