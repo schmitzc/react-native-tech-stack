a80cc2588896c9beb8b4ecb1bab11dedaad53a8a
Cassie Koomjian <schmitzc@gmail.com>
2016-10-05 12:35:53 -0700

    Add selectLibrary action creator

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
@@ -20,4 +22,4 @@ const styles = {
   },
 };
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
