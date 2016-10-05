3f39e645ce5041a299be738821f6638f4717b9d8
Cassie Koomjian <schmitzc@gmail.com>
2016-10-05 12:19:49 -0700

    Add ListView for list of libraries

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
+import ListItem from './ListItem';
+
 class LibraryList extends Component {
+  componentWillMount() {
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2
+    });
+
+    this.dataSource = dataSource.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem library={library} />;
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
