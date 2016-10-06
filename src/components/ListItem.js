5fe243bfc822a7a3fc5e1b1e9b03a6206d20fc95
Cassie Koomjian <schmitzc@gmail.com>
2016-10-06 12:23:26 -0700

    Show description when card title is pressed

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, TouchableWithoutFeedback, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
+    const { id, title } = this.props.library;
+
     return (
-      <CardSection>
-        <Text style={styles.title}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={styles.title}>{title}</Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -22,4 +41,10 @@ const styles = {
   },
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
